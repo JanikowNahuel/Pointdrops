2a6dda0987483be1a8aef9ba3e4990b9eca213e6
JanikowNahuel <102630994+JanikowNahuel@users.noreply.github.com>
2025-05-09 03:52:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -64,31 +64,16 @@ function filterByCategory(categoria) {
 
 function showAddProductForm() {
     document.getElementById('add-form').style.display = 'block';
-    document.getElementById("brand-select").addEventListener("change", loadProducts);
-
 }
 
 async function addProduct() {
     const descripcion = document.getElementById('product-description').value;
     const nombre = document.getElementById('product-name').value;
-    const selectedCategory = document.getElementById("category-select").value;
-    const selectedBrand = document.getElementById("brand-select").value;
-
-    let query = supabase.from("productos").select("*");
-
-    if (selectedCategory !== "all") {
-    query = query.eq("categoria", selectedCategory);
-    }
-    if (selectedBrand !== "all") {
-    query = query.eq("marca", selectedBrand);
-    }
-
-    const { data, error } = await query;
-
+    const categoria = document.getElementById('product-category').value;
     const fileInput = document.getElementById('product-image');
     const file = fileInput.files[0];
 
-    if (!nombre || !file) {
+    if (!nombre || !categoria || !file) {
         return alert('Completa todos los campos y selecciona una imagen');
     }
 
@@ -216,8 +201,9 @@ async function checkAdmin() {
     }
 }
 
-
+document.getElementById('category-select').addEventListener('change', (e) => {
+    filterByCategory(e.target.value);
+});
 
 
 window.filterByCategory = filterByCategory;
-
